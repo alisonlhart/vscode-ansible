5d4ec926a4601b0390e619af6502194e257f35cb
Ali <alhart@redhat.com>
2024-08-01 15:25:25 -0400

    Discover ADT walkthrough

---
+++ test/ui-test/walkthroughUiTest.ts
@@ -0,0 +1,23 @@
+import { expect, config } from "chai";
+import {
+  Workbench,
+  BottomBarPanel,
+  VSBrowser,
+} from "vscode-extension-tester";
+import { sleep } from "./uiTestHelper";
+
+config.truncateThreshold = 0;
+export function walkthroughUiTest(): void {
+  describe("Verify Discover ADT walkthrough page elements", async () => {
+    
+
+
+  })
+
+
+
+
+
+
+
+}
