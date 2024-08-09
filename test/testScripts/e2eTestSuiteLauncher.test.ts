f129ee3e624a7d27dd70b1c7c580b501d8f2aba6
alisonlhart <alhart@redhat.com>
2024-08-09 16:30:34 -0400

    Retesting

--- test/testScripts/e2eTestSuiteLauncher.test.ts
+++ test/testScripts/e2eTestSuiteLauncher.test.ts
@@ -14,47 +14,47 @@ import { testExtensionForFilesOutsideWorkspace } from "./outsideWorkspace/testEx
 describe("END-TO-END TEST SUITE FOR REDHAT.ANSIBLE EXTENSION", () => {
   const run_lightspeed_tests_only =
     process.env.RUN_LIGHTSPEED_TESTS_ONLY || "0";
-  describe("TEST EXTENSION IN LOCAL ENVIRONMENT", () => {
-    before(async () => {
-      setFixtureAnsibleCollectionPathEnv(
-        "/home/runner/.ansible/collections:/usr/share/ansible/collections",
-      );
-      await updateSettings("trace.server", "verbose", "ansibleServer");
-    });
+//   describe("TEST EXTENSION IN LOCAL ENVIRONMENT", () => {
+//     before(async () => {
+//       setFixtureAnsibleCollectionPathEnv(
+//         "/home/runner/.ansible/collections:/usr/share/ansible/collections",
+//       );
+//       await updateSettings("trace.server", "verbose", "ansibleServer");
+//     });
 
-    after(async () => {
-      await updateSettings("trace.server", "off", "ansibleServer"); // Revert back the default settings
-    });
+//     after(async () => {
+//       await updateSettings("trace.server", "off", "ansibleServer"); // Revert back the default settings
+//     });
 
-    if (run_lightspeed_tests_only !== "1") {
-      testHoverWithoutEE();
-      testDiagnosticsAnsibleWithoutEE();
-      testDiagnosticsYAMLWithoutEE();
-    }
-    testLightspeed();
-  });
+//     if (run_lightspeed_tests_only !== "1") {
+//       testHoverWithoutEE();
+//       testDiagnosticsAnsibleWithoutEE();
+//       testDiagnosticsYAMLWithoutEE();
+//     }
+//     testLightspeed();
+//   });
 
-   const skip_ee = process.env.SKIP_PODMAN || process.env.SKIP_DOCKER || "0";
-  if (skip_ee !== "1" && run_lightspeed_tests_only !== "1") {
-    describe("TEST EXTENSION IN EXECUTION ENVIRONMENT", () => {
-      before(async () => {
-        setFixtureAnsibleCollectionPathEnv(
-          "/home/runner/.ansible/collections:/usr/share/ansible/collections",
-        );
-        await enableExecutionEnvironmentSettings();
-      });
+//    const skip_ee = process.env.SKIP_PODMAN || process.env.SKIP_DOCKER || "0";
+//   if (skip_ee !== "1" && run_lightspeed_tests_only !== "1") {
+//     describe("TEST EXTENSION IN EXECUTION ENVIRONMENT", () => {
+//       before(async () => {
+//         setFixtureAnsibleCollectionPathEnv(
+//           "/home/runner/.ansible/collections:/usr/share/ansible/collections",
+//         );
+//         await enableExecutionEnvironmentSettings();
+//       });
 
-      after(async () => {
-        await disableExecutionEnvironmentSettings(); // Revert back the default settings
-      });
+//       after(async () => {
+//         await disableExecutionEnvironmentSettings(); // Revert back the default settings
+//       });
 
-      testHoverEE();
-    });
-  }
+//       testHoverEE();
+//     });
+//   }
 
   if (run_lightspeed_tests_only !== "1") {
     describe("TEST EXTENSION FOR FILES OUTSIDE WORKSPACE", function () {
       testExtensionForFilesOutsideWorkspace();
-    });
+    }); 
   }
 });
